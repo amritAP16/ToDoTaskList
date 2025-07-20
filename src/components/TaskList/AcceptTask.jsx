f2404a36cbefe46da793e307c0b35e2123f8bd07
CurseU10 <pandeyamrit961@gmail.com>
2025-07-21 00:13:02 +0530

    completed employee UI and dynamically added taskTitle, taskDescription, taskDate,taskPriority,taskCategory and also color accotding to the priority level

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const AcceptTask = () => {
+const AcceptTask = ({data}) => {
   return (
      <div  className='flex-shrink-0 w-80 h-full bg-red-400 rounded-2xl'>
             <div className='flex justify-between p-5'>
-                <h3 className='bg-red-600 px-3 py-1 rounded-lg'>High</h3>
-                <h4 className='text-sm py-1.5'>20 feb 2025</h4>
+                <h3 className={`px-3 py-1 rounded-lg ${data.priority === "high"? "bg-red-600": data.priority === "medium" ? "bg-yellow-600" : "bg-green-600" }`}>{data.priority}</h3>
+                <h3 className='bg-cyan-950  px-3 py-1 rounded-lg'>{data.category}</h3>
+                <h4 className={`px-3 py-1 rounded-lg ${data.priority === "high"? "bg-red-600": data.priority === "medium" ? "bg-yellow-600" : "bg-green-600" }`}>{data.date}</h4>
             </div>
-            <h2 className='mt-3 text-xl px-3 font-semibold'>Complete Your HomeWork.</h2>
+            <h2 className='mt-3 text-xl px-3 font-semibold'>{data.title}</h2>
             <p className='text-sm px-3 mt-1'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, non minus magnam consequuntur soluta placeat culpa.
+                {data.description}
             </p>
             <div className='flex items-start justify-between p-4'>
                 <button className='bg-green-600 px-2 rounded-md text-sm'>Mark as Completed</button>
--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const NewTask = () => {
+const NewTask = ({data}) => {
   return (
     <div  className='flex-shrink-0 w-80 h-full bg-green-400 rounded-2xl'>
             <div className='flex justify-between p-5'>
-                <h3 className='bg-red-600 px-3 py-1 rounded-lg'>High</h3>
-                <h4 className='text-sm py-1.5'>20 feb 2025</h4>
+                <h3 className={`px-3 py-1 rounded-lg ${data.priority === "high"? "bg-red-600": data.priority === "medium" ? "bg-yellow-600" : "bg-green-600" }`}>{data.priority}</h3>
+                <h3 className='bg-cyan-950 px-3 py-1 '>{data.category}</h3>
+                <h4 className={`px-3 py-1 rounded-lg ${data.priority === "high"? "bg-red-600": data.priority === "medium" ? "bg-yellow-600" : "bg-green-600" }`}>{data.date}</h4>
             </div>
-            <h2 className='mt-3 text-xl px-3 font-semibold'>Complete Your HomeWork.</h2>
+            <h2 className='mt-3 text-xl px-3 font-semibold'>{data.title}</h2>
             <p className='text-sm px-3 mt-1'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, non minus magnam consequuntur soluta placeat culpa.
+              {data.description}
             </p>
             <div className='mt-2 p-2'>
                 <button className='w-full flex items-end justify-center'>Accept Task</button>
